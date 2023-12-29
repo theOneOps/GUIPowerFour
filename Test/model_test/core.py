e7f7feb82a5be2c802238def040a68490730e534
theOneOps <142930985+theOneOps@users.noreply.github.com>
2023-12-29 02:10:11 +0100

    adding of the .travis.yml, the .pre-commit-config.yaml, and the requirements.txt for the CI/CD process on the project

--- Test/model_test/core.py
+++ Test/model_test/core.py
@@ -22,10 +22,14 @@ class MyCoreTest(unittest.TestCase):
             [1, -1, -1, -1, -1],
         ]
 
-        self.assertTrue(model.core.finish_range_horizontal(1, 0, 0,
-                                                           grid, 3, 5, 5))
-        self.assertFalse(model.core.finish_range_horizontal(0, 2, 1,
-                                                            grid, 3, 5, 5))
+        (self.assertTrue(model.core.finish_range_horizontal(1,
+                                                            0, 0,
+                                                            grid, 3,
+                                                            5,
+                                                            5)))
+        self.assertFalse(
+            model.core.finish_range_horizontal(0, 2, 1, grid,
+                                               3, 5, 5))
 
     def test_finish_range_vertical(self):
         # Create a sample grid
@@ -38,10 +42,14 @@ class MyCoreTest(unittest.TestCase):
             [-1, -1, -1, -1, -1],
         ]
 
-        self.assertTrue(model.core.finish_range_vertical(1, 3, 0, grid,
-                                                         4, 5, 5))
-        self.assertFalse(model.core.finish_range_vertical(0, 2, 1, grid,
-                                                          4, 5, 5))
+        self.assertTrue(
+            model.core.finish_range_vertical(1, 3, 0,
+                                             grid, 4, 5,
+                                             5))
+        self.assertFalse(
+            model.core.finish_range_vertical(0, 2,
+                                             1, grid, 4, 5,
+                                             5))
 
     def test_finish_range_bas_gauche(self):
         # Create a sample grid
@@ -53,10 +61,13 @@ class MyCoreTest(unittest.TestCase):
             [0, 1, 1, 0, 1],
         ]
 
-        self.assertTrue(model.core.finish_range_bas_gauche(0, 0, 4, grid,
-                                                           5, 5, 5))
-        self.assertFalse(model.core.finish_range_bas_gauche(1, 1, 1, grid,
-                                                            5, 5, 5))
+        self.assertTrue(
+            model.core.finish_range_bas_gauche(0, 0,
+                                               4, grid, 5,
+                                               5, 5))
+        self.assertFalse(
+            model.core.finish_range_bas_gauche(1, 1, 1, grid,
+                                               5, 5, 5))
 
     def test_finish_range_haut_gauche(self):
         # Create a sample grid
@@ -68,10 +79,12 @@ class MyCoreTest(unittest.TestCase):
             [-1, -1, -1, 0, 1],
         ]
 
-        self.assertTrue(model.core.finish_range_haut_gauche(1, 1, 1, grid,
-                                                            5, 5, 5))
-        self.assertFalse(model.core.finish_range_haut_gauche(0, 0, 2, grid,
-                                                             5, 5, 5))
+        self.assertTrue(
+            model.core.finish_range_haut_gauche(1, 1, 1, grid,
+                                                5, 5, 5))
+        self.assertFalse(
+            model.core.finish_range_haut_gauche(0, 0, 2, grid,
+                                                5, 5, 5))
 
     def test_finish_range_in_all_directions(self):
         # Create a sample grid
@@ -88,10 +101,14 @@ class MyCoreTest(unittest.TestCase):
         # true means there is either a victory or draw
 
         self.assertTrue(
-            model.core.finish_range_in_all_directions(1, 4, 0, grid, 5, 5, 5)
+            model.core.finish_range_in_all_directions(1, 4, 0,
+                                                      grid,
+                                                      5, 5, 5)
         )
         self.assertFalse(
-            model.core.finish_range_in_all_directions(1, 0, 1, grid, 5, 5, 5)
+            model.core.finish_range_in_all_directions(1, 0, 1,
+                                                      grid,
+                                                      5, 5, 5)
         )
 
     def test_play_bot(self):
@@ -106,10 +123,12 @@ class MyCoreTest(unittest.TestCase):
         # false means there is a victory
         # true means there is either a victory or draw
 
-        self.assertTrue(model.core.play_bot(grid, 0, True, 5, 5,
-                                            5, [2, 0]))
-        self.assertFalse(model.core.play_bot(grid, 0, True, 5, 5,
-                                             5, [4, 0]))
+        self.assertTrue(model.core.play_bot(grid, 0, True,
+                                            5,
+                                            5, 5, [2, 0]))
+        self.assertFalse(model.core.play_bot(grid, 0, True,
+                                             5,
+                                             5, 5, [4, 0]))
 
     def test_play_human(self):
         grid = [
@@ -120,9 +139,11 @@ class MyCoreTest(unittest.TestCase):
             [1, 1, 1, 0, 1],
         ]
 
-        self.assertTrue(model.core.play_human(grid, 1, [1, 1], True, 5,
+        self.assertTrue(model.core.play_human(grid, 1, [1, 1],
+                                              True, 5,
                                               5, 5))
-        self.assertFalse(model.core.play_human(grid, 1, [0, 0], True, 5,
+        self.assertFalse(model.core.play_human(grid, 1, [0, 0],
+                                               True, 5,
                                                5, 5))
 
     def test_peek(self):
@@ -144,18 +165,18 @@ class MyCoreTest(unittest.TestCase):
         # testing the tourJeu value to match player's round
 
         # human's tour
-        self.assertFalse(model.core.launch_game(grid, 5, 5, 8, 5, True,
-                                                [0, 0]))
+        self.assertFalse(model.core.launch_game(grid, 5, 5, 8, 5,
+                                                True, [0, 0]))
         # bot's tour
-        self.assertFalse(model.core.launch_game(grid, 5, 5, 5, 5, True,
-                                                [0, 4]))
+        self.assertFalse(model.core.launch_game(grid, 5, 5, 5, 5,
+                                                True, [0, 4]))
 
         # human's tour
-        self.assertTrue(model.core.launch_game(grid, 5, 5, 2, 5, True,
-                                               [1, 1]))
+        self.assertTrue(model.core.launch_game(grid, 5, 5, 2, 5,
+                                               True, [1, 1]))
         # Bot's tour
-        self.assertTrue(model.core.launch_game(grid, 5, 5, 17, 5, True,
-                                               [0, 1]))
+        self.assertTrue(model.core.launch_game(grid, 5, 5, 17, 5,
+                                               True, [0, 1]))
 
 
 if __name__ == "__main__":
